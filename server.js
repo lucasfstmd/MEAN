381407f013a565265b7e7d6855985bca65ad906a
lucasfstmd <100228530+lucasfstmd@users.noreply.github.com>
2023-02-01 10:45:31 -0300

    Ligando server, adicionado dependencia nodemon

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const path = require(path);
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.get('/', function (req, res){
+    res.json({message:'Hellow-Word'})
+});
+
+app.listen(port, function (){
+    console.log(`server runing on port ${port}`)
+})
